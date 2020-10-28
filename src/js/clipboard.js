10e04fc8dda9dcdba38d92ea0d376988dcf520f4
ijun17 <54887575+ijun17@users.noreply.github.com>
2020-10-28 11:07:31 +0900

    Update clipboard.js

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -3,6 +3,7 @@ function setClipboard(text){
     document.body.appendChild(dummy);
     dummy.value = text;
     dummy.select();
+    console.log("clipboard.setClipboard : ", text, " ", dummy.value);
     document.execCommand("copy");
     document.body.removeChild(dummy);
 }
