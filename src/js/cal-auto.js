0b0823fba50a3a583801309ee1f212c4fff4bf5d
ijun17 <54887575+ijun17@users.noreply.github.com>
2020-11-02 10:12:39 +0900

    Add files via upload

--- src/js/cal-auto.js
+++ src/js/cal-auto.js
@@ -7,42 +7,53 @@ const err2 = document.querySelector(".js-err2"),
     newME2 = document.querySelector(".js-newMathExp2"),
     result2 = document.querySelector(".js-result2");
 
-
-function isValid2(ch){
-    return ((ch>='0'&&ch<='9')||ch=="*"||ch=="/"||ch=="+"||ch=="-"||ch=="."
-    ||ch=="("||ch==")"||ch=="{"||ch=="}"||ch=="["||ch=="]");
+function isValid2(ch) {
+    return ((ch >= '0' && ch <= '9') || ch == "*" || ch == "/" || ch == "+" || ch == "-" || ch == "."
+        || ch == "(" || ch == ")" || ch == "{" || ch == "}" || ch == "[" || ch == "]");
 }
 
-function makeMathExp2(oldMathExp){
-    let newMathExp="";
-    for(let i=0; i<oldMathExp.length; i++){
-        if(isValid2(oldMathExp[i]))newMathExp+=oldMathExp[i];
+function makeMathExp2(oldMathExp) {
+    let newMathExp = "";
+    for (let i = 0; i < oldMathExp.length; i++) {
+        if (isValid2(oldMathExp[i])) newMathExp += oldMathExp[i];
     }
     return newMathExp;
 }
 
-function handlerSubmit2(event){
-    event.preventDefault();
-    const currentValue = me2.value;
-    let newMathExp = makeMathExp2(currentValue);
-    let resultME=-1;
-    try{
-        resultME = eval(newMathExp);
-    }catch(e){
-        console.log("err");
-    }
-    
-    //출력
-    me2.value="";
-    oldME2.innerText = currentValue;
-    newME2.innerText = `=${newMathExp}`;
-    result2.innerText = resultME;
-    
-    setClipboard(`=${newMathExp}`);
+function setClipboard(text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    console.log("clipboard.setClipboard : ", text, " ", dummy.value);
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
 }
 
-function init2(){
-    form2.addEventListener("submit", handlerSubmit2);
+function handlerSubmit2(event) {
+    setTimeout(function () {
+        //event.preventDefault();
+        const currentValue = me2.value;
+        let newMathExp = makeMathExp2(currentValue);
+        let resultME = -1;
+        try {
+            resultME = eval(newMathExp);
+        } catch (e) {
+            console.log("err");
+        }
+        //출력
+        me2.value = "";
+        oldME2.innerText = currentValue;
+        newME2.innerText = `=${newMathExp}`;
+        result2.innerText = resultME;
+        setClipboard(`=${newMathExp}`);
+    }, 100);
+}
+
+function init2() {
+    form2.addEventListener("paste", handlerSubmit2);
 }
 
 init2();
+
+//input : 
--- src/js/cal-subWindow.js
+++ src/js/cal-subWindow.js
@@ -1,3 +1,4 @@
+
 //입력
 const me = document.querySelector(".js-mathExp"),
     form = document.querySelector(".js-form");
@@ -7,50 +8,62 @@ const err = document.querySelector(".js-err"),
     newME = document.querySelector(".js-newMathExp"),
     result = document.querySelector(".js-result");
 
-
-
-function isValid(ch){
-    return ((ch>='0'&&ch<='9')||ch=="*"||ch=="/"||ch=="+"||ch=="-"||ch=="."
-    ||ch=="("||ch==")"||ch=="{"||ch=="}"||ch=="["||ch=="]");
+function isValid(ch) {
+    return ((ch >= '0' && ch <= '9') || ch == "*" || ch == "/" || ch == "+" || ch == "-" || ch == "."
+        || ch == "(" || ch == ")" || ch == "{" || ch == "}" || ch == "[" || ch == "]");
 }
 
-function makeMathExp(oldMathExp){
-    let newMathExp="";
+function makeMathExp(oldMathExp) {
+    let newMathExp = "";
     let isValidRange = false;
-    for(let i=0; i<oldMathExp.length; i++){
-        if(oldMathExp[i]==","){
-            isValidRange=false;
-            newMathExp+="+";
+    for (let i = 0; i < oldMathExp.length; i++) {
+        if (oldMathExp[i] == ",") {
+            isValidRange = false;
+            newMathExp += "+";
         }
-        if(isValidRange && isValid(oldMathExp[i]))newMathExp+=oldMathExp[i];
-        if(oldMathExp[i]=="=")isValidRange=true;
+        if (isValidRange && isValid(oldMathExp[i])) newMathExp += oldMathExp[i];
+        if (oldMathExp[i] == "=") isValidRange = true;
     }
     return newMathExp;
 }
 
-function handlerSubmit(event){
-    event.preventDefault();
-    const currentValue = me.value;
-    let newMathExp = makeMathExp(currentValue);
-    let resultME=-1;
-    try{
-        resultME = eval(newMathExp);
-    }catch(e){
-        console.log("err");
-    }
-    
-    //출력
-    me.value="";
-    oldME.innerText = currentValue;
-    newME.innerText = `=${newMathExp}`;
-    result.innerText = resultME;
-    
-    setClipboard(`=${newMathExp}`);
-}   
+function setClipboard(text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    console.log("clipboard.setClipboard : ", text, " ", dummy.value);
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
 
-function init(){
-    form.addEventListener("submit", handlerSubmit);
-    
+function handlerSubmit(event) {
+    //event.preventDefault();
+    setTimeout(function () {
+        console.log("window : handler me.value : " + me.value);
+        const currentValue = me.value;
+        console.log("window : handler curentValue : " + currentValue);
+        let newMathExp = makeMathExp(currentValue);
+        let resultME = -1;
+        try {
+            resultME = eval(newMathExp);
+        } catch (e) {
+            console.log("err");
+        }
+        //출력
+        me.value = "";
+        oldME.innerText = currentValue;
+        newME.innerText = `=${newMathExp}`;
+        result.innerText = resultME;
+        setClipboard(`=${newMathExp}`);
+    }, 100);
+}
+
+function init() {
+    form.addEventListener("paste", handlerSubmit);
 }
 
 init();
+
+//input : 33A/33=34*23, sfs=2*3, 38e3 = 34*1
+//output : =34*23+2*3+34*1
