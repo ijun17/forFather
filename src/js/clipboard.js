2ba88abba047ae14230203308247ab64b2a25ea3
ijun17 <54887575+ijun17@users.noreply.github.com>
2020-11-02 10:12:12 +0900

    Delete clipboard.js

--- src/js/clipboard.js
+++
@@ -1,9 +0,0 @@
-function setClipboard(text){
-    var dummy = document.createElement("textarea");
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    console.log("clipboard.setClipboard : ", text, " ", dummy.value);
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-}
